7b99022d5d497449c408e97d150b6b00c04029c6
riqzamuqtada <riqmuq106@gmail.com>
2023-11-22 09:14:26 +0700

    Initial commit

---
+++ app/src/main/java/com/kospin/myapplication/LoadingActivity.kt
@@ -0,0 +1,11 @@
+package com.kospin.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
