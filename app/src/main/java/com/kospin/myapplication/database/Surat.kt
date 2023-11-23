e5c1a9471e873ddfd6c0e4817e155ee28aef378b
riqzamuqtada <riqmuq106@gmail.com>
2023-11-23 10:16:30 +0700

    database

---
+++ app/src/main/java/com/kospin/myapplication/database/Surat.kt
@@ -0,0 +1,19 @@
+package com.kospin.myapplication.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("tb_surat")
+data class Surat (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo("id")       val id          : Int,
+    @ColumnInfo("no_surat") val no_surat    : String,
+    @ColumnInfo("hal")      val hal         : String,
+    @ColumnInfo("jenis")    val jenis       : String,
+    @ColumnInfo("divisi")   val divisi      : String,
+    @ColumnInfo("tanggal")  val tanggal     : String,
+    @ColumnInfo("catatan")  val catatan     : String,
+    @ColumnInfo("gambar")   val gambar      : String
+
+    )
